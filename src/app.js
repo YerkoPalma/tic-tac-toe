360c75545577c06b807b74380d60f8e308c6aaff
YerkoPalma <yerko.palma@usach.cl>
2016-07-27 23:18:52 -0400

    improved build process

--- src/app.js
+++ src/app.js
@@ -2,17 +2,15 @@ const choo = require('choo')
 const sf = require('sheetify')
 const mainView = require('./views/main')
 const gameView = require('./views/game')
-const chooLog = require('choo-log')
 
 sf('normalize.css')
 sf('./style.css', { global: true })
 
-const logger = chooLog()
-const app = choo({
-  onAction: logger.onAction,
-  onError: logger.onError,
-  onStateChange: logger.onStateChange
-})
+const app = choo()
+if (process.env.NODE_ENV !== 'production') {
+  const log = require('choo-log')
+  app.use(log())
+}
 
 app.model(require('./models/board'))
 app.model(require('./models/player'))
