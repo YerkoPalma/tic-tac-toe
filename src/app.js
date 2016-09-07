e5744bb330da87f823e7537fc4d92b08edfb17d8
Yerko Palma <yerko.palma@usach.cl>
2016-09-07 13:34:26 +0000

    sw added for production

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+/* global navigator */
 const choo = require('choo')
 const sf = require('sheetify')
 const mainView = require('./views/main')
@@ -22,3 +23,29 @@ app.router(route => [
 
 const tree = app.start({ hash: true })
 document.body.appendChild(tree)
+
+if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
+  navigator.serviceWorker.register('service-worker.js').then(function (reg) {
+    reg.onupdatefound = function () {
+      var installingWorker = reg.installing
+
+      installingWorker.onstatechange = function () {
+        switch (installingWorker.state) {
+          case 'installed':
+            if (navigator.serviceWorker.controller) {
+              console.log('New or updated content is available.')
+            } else {
+              console.log('Content is now available offline!')
+            }
+            break
+
+          case 'redundant':
+            console.error('The installing service worker became redundant.')
+            break
+        }
+      }
+    }
+  }).catch(function (e) {
+    console.error('Error during service worker registration:', e)
+  })
+}
