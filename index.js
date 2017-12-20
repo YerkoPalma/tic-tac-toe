db065f64c5286726d515dffd8190abc6a15d0cd4
Yerko Palma <yerko.palma@usach.cl>
2017-12-20 01:38:34 +0000

    upgrading

---
+++ index.js
@@ -0,0 +1,17 @@
+var choo = require('choo')
+var css = require('sheetify')
+
+css('tachyons')
+css('./style.css', { global: true })
+
+var app = choo()
+if (process.env.NODE_ENV !== 'production') {
+  app.use(require('choo-devtools')())
+} else {
+  app.use(require('choo-service-worker')())
+}
+
+app.route('/', require('./views/main')),
+app.route('/game', require('./views/game'))
+app.route('/*', require('./views/notFound'))
+app.mount('body')
---
+++ views/main.js
@@ -0,0 +1,41 @@
+var html = require('choo/html')
+
+var mainView = (state, emit) => {
+  return html`
+    <div>
+      <header class="tc pv3 pv4-ns cf">
+        <h1 class="f2 f2-ns fw6 mid-gray">Tic tac toe powered by choo</h1>
+      </header>
+      <div class="wrapper center">
+        <div class="flex center pv3 w-80 w-40-l w-50-ns w-80-m">
+          <input id="player" type="text" class="outline-0 outline-transparent input-reset pa3 br4 br--left input-reset b--gray ba w-60 w-75-l w-75-m w-75-ns pa3 br4 br--left">
+          <button onclick=${e => emit('player:join')} class="br4 pointer outline-0 W-40 w-25-l w-25-m w-25-ns bl-0 br--right pa3 dim b--gray ba bg-washed-yellow hover-bg-yellow fw7 dark-gray tracked-mega">Play</button>
+        </div>
+      </div>
+      <div class="w-100">
+        <table class="collapse ba b--black-10 pv2 center w-80 w-25-l w-33-ns w-50-m">
+          <thead>
+            <tr>
+              <th class="pv2 ph3 tl f6 fw6 ttu">#</th>
+              <th class="pv2 ph3 tl f6 fw6 ttu">Username</th>
+              <th class="pv2 ph3 tl f6 fw6 ttu">Score</th>
+            </tr>
+          </thead>
+          <tbody>
+          ${state.player.users.map((user, i) => {
+            return html`
+              <tr class="striped--near-white">
+                <td class="pv2 ph3">${i + 1}</td>
+                <td class="pv2 ph3">${user.name}</td>
+                <td class="pv2 ph3">${user.score}</td>
+              </tr>
+            `
+          })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  `
+}
+
+module.exports = mainView
