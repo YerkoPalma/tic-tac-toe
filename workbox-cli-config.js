b298440072f6185bff6c40be06304e7dd8f08dcb
Yerko Palma <yerko.palma@usach.cl>
2018-01-03 13:14:36 +0000

    deploy to now and add service worker

---
+++ workbox-cli-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "globDirectory": "./",
+  "globPatterns": [
+    "**/*.{js,json,html,css}"
+  ],
+  "swDest": "sw.js",
+  "globIgnores": [
+    "workbox-cli-config.js"
+  ]
+};
